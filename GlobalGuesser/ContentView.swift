aa93cdfd630870fa8656ea8bbd5ed3f1cc3322f2
Ethan Humphrey <ethantheinnovator@gmail.com>
2020-01-07 07:39:30 -0500

    Initial Commit

---
+++ GlobalGuesser/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GlobalGuesser
+//
+//  Created by Ethan Humphrey on 1/7/20.
+//  Copyright © 2020 Ethan Humphrey. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
